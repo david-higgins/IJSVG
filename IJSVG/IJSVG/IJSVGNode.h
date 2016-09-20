5ecc626f069972064920320ddc72eb0412043ae0
Paul Broadbent <paul@reincubate.com>
2016-09-20 14:20:12 +0100

    added dependencies

--- IJSVG/IJSVG/IJSVGNode.h
+++ IJSVG/IJSVG/IJSVGNode.h
@@ -7,6 +7,7 @@
 //
 
 #import <Foundation/Foundation.h>
+#import <Cocoa/Cocoa.h>
 #import "IJSVGStyle.h"
 
 @class IJSVGDef;
--- IJSVG/IJSVG/IJSVGPath.h
+++ IJSVG/IJSVG/IJSVGPath.h
@@ -7,6 +7,7 @@
 //
 
 #import <Foundation/Foundation.h>
+#import <Cocoa/Cocoa.h>
 #import "IJSVGBezierPathAdditions.h"
 #import "IJSVGNode.h"
 
