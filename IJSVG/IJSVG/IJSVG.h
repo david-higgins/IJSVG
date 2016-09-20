25bd82955b586d2efaace23782a4bb8712e2b0bc
Paul Broadbent <paul@reincubate.com>
2016-09-20 14:19:08 +0100

    created proj and moved files

--- IJSVG/IJSVG/IJSVG.h
+++ IJSVG/IJSVG/IJSVG.h
@@ -1,3 +1,15 @@
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for IJSVG.
+FOUNDATION_EXPORT double IJSVGVersionNumber;
+
+//! Project version string for IJSVG.
+FOUNDATION_EXPORT const unsigned char IJSVGVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <IJSVG/PublicHeader.h>
+
+
 //
 //  IJSVGImage.h
 //  IconJar
